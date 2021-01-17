c4bc4ba97b899dcd2f9efe461f11d2a99d42100a
eduardo.beninca <eduardo.beninca@dataprev.gov.br>
2021-01-17 17:02:31 +0000

    day01 finished

---
+++ 2020/day01/day01.py
@@ -0,0 +1,21 @@
+import os
+
+with open(os.path.dirname(__file__) + '/day01input.txt') as f:
+    numbers = [int(line) for line in f]
+
+for n1 in numbers:
+    for n2 in numbers:
+        if n1 == n2:
+            continue
+        if n1 + n2 == 2020:
+            print(n1, n2, n1 + n2, n1 * n2)
+
+for n1 in numbers:
+    for n2 in numbers:
+        if n1 == n2:
+            continue
+        for n3 in numbers:
+            if n1 == n3 or n2 == n3:
+                continue
+            if n1 + n2 + n3 == 2020:
+                print(n1, n2, n3, n1 + n2 + n3, n1 * n2 * n3)
